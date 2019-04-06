d8714a1a2359548c7303d6b3a771c0120215ee7f
unknown <murilo001os@gmail.com>
2019-04-06 12:17:44 -0300

    Hover - icones no menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 //var instance = M.Collapsible.getInstance(document.querySelectorAll('#abrir-check'));
 //instance.open(0);
-$(document).ready(function(){
+$(document).ready(function () {
 	$("#hidden").hide();
 
 
-	$('#btn-check-list').click(function() {
+	$('#btn-check-list').click(function () {
 		var clicks = $(this).data('clicks');
 		if (clicks) {
 			$("#hidden").hide();
@@ -20,28 +20,36 @@ $(document).ready(function(){
 
 });
 
-
+function trocaCorIcon() {
+	/** 
+	$("#label-curso").mouseover(function () {
+		//$("#icon-curso").attr("class", "colored-blue");
+		//$("#icon-curso").css("color", "red");
+		alert("grab thing");
+	});
+	*/
+}
 
 
 function myFunction() {
-  // Declare variables 
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
+	// Declare variables 
+	var input, filter, table, tr, td, i, txtValue;
+	input = document.getElementById("myInput");
+	filter = input.value.toUpperCase();
+	table = document.getElementById("myTable");
+	tr = table.getElementsByTagName("tr");
 
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-  	td = tr[i].getElementsByTagName("td")[0];
-  	if (td) {
-  		txtValue = td.textContent || td.innerText;
-  		if (txtValue.toUpperCase().indexOf(filter) > -1) {
-  			tr[i].style.display = "";
-  		} else {	
-  			tr[i].style.display = "none";
-  		}
-  	} 
-  }
+	// Loop through all table rows, and hide those who don't match the search query
+	for (i = 0; i < tr.length; i++) {
+		td = tr[i].getElementsByTagName("td")[0];
+		if (td) {
+			txtValue = td.textContent || td.innerText;
+			if (txtValue.toUpperCase().indexOf(filter) > -1) {
+				tr[i].style.display = "";
+			} else {
+				tr[i].style.display = "none";
+			}
+		}
+	}
 }
 
